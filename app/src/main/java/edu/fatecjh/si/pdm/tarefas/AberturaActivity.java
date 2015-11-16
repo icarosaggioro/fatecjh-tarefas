4ed513211e2324a8b8a7a9132539d8663ccb6fca
Icaro Saggioro <icarosaggioro@bitbucket.org>
2015-11-16 14:14:08 -0200

    Suggestion on how to pre-load data into the database

--- app/src/main/java/edu/fatecjh/si/pdm/tarefas/AberturaActivity.java
+++ app/src/main/java/edu/fatecjh/si/pdm/tarefas/AberturaActivity.java
@@ -1,8 +1,11 @@
 package edu.fatecjh.si.pdm.tarefas;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import edu.fatecjh.si.pdm.tarefas.persistence.TarefaDAO;
 
 public class AberturaActivity extends AppCompatActivity {
 
@@ -35,6 +38,27 @@ public class AberturaActivity extends AppCompatActivity {
         // start thread
         background.start();
 
+        popularTabelaTarefa(this);
+
+    }
+
+    private void popularTabelaTarefa(Context ctx) {
+        TarefaDAO dao = TarefaDAO.getInstance(ctx);
+        Tarefa tar;
+
+        tar = new Tarefa();
+        tar.titulo = "Tarefa Teste 1";
+        tar.descricao = "Descricao da Tarefa Teste 1";
+        tar.tipo = TipoTarefa.FACULDADE;
+        tar.status = StatusTarefa.NOVA;
+        dao.criar(tar);
+
+        tar = new Tarefa();
+        tar.titulo = "Tarefa Teste 2";
+        tar.descricao = "Descricao da Tarefa Teste 2";
+        tar.tipo = TipoTarefa.SONHOS_FUTUROS;
+        tar.status = StatusTarefa.NOVA;
+        dao.criar(tar);
 
     }
 
